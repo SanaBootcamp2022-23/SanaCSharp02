77a7138e52400c750502ba58cf7142f605bd026b
Ivan Savchuk <ipzm222_sio@student.ztu.edu.ua>
2022-12-21 23:50:44 +0200

    Task 4 (While1) done

---
+++ SanaCSharp02/While1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+int n;
+double resultA = 0;
+
+Console.Write("n = ");
+n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+    resultA += Math.Pow(-1, i) / (2 * i + 1);
+
+Console.WriteLine($"a = {resultA}");
+
+double resultB = 0;
+
+for (int i = 1; i <= n; i++)
+    resultB += 1 + (1 / Math.Pow(i, 2));
+
+Console.WriteLine($"b = {resultB}");
+
+double resultC = 0;
+double factor = 1;
+
+for (int i = 1; i <= n; i++)
+{
+    factor *= i;
+    resultC += factor;
+}
+
+Console.WriteLine($"c = {resultC}");
+
