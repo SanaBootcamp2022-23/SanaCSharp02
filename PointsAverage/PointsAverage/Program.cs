523bf3ea674c1389d6d3317af55a64ec0daa51bc
VitaBrrevis <vitalina.doronina@gmail.com>
2022-12-21 17:52:30 +0200

    seems to be working

---
+++ PointsAverage/PointsAverage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double sum =0, average;
+        for (int i = 1; i < 6; i++)
+        {
+            Console.WriteLine($"Enter the {i} mark");
+            Console.Write("-> ");
+            sum += double.Parse(Console.ReadLine());
+        }
+        average = sum / 5;
+        if (average >= 4)
+            Console.WriteLine("dopusk");
+        else
+            Console.WriteLine("No dopusk");
+    }
+}
