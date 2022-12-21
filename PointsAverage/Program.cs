6065ffa9b0c2a8f9695ec7591ee605b9c9d20e3c
Bohdan Andriichuk <sana22_abv@student.ztu.edu.ua>
2022-12-21 17:21:51 +0200

    works

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,6 @@
+﻿double sum = 0;
+for (int i = 0; i < 5; i++)
+    sum += int.Parse(Console.ReadLine());
+sum /= 5;
+if (sum >= 4) Console.WriteLine($"Допущений. {sum}");
+else Console.WriteLine($"Не допущений. {sum}");
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+double D = Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+if (D > 0)
+{
+    Console.WriteLine($"x1={(-b + D) / (2 * a)}");
+    Console.WriteLine($"x2={(-b - D) / (2 * a)}");
+}
+else if (D == 0) Console.WriteLine($"x={-b / (2 * a)}");
+else Console.WriteLine("D<0");
