a2ce06cba66c01cb2d3b29f922d844de698cc311
Maxim Shyryaev <ipz223_shmye@student.ztu.edu.ua>
2022-12-21 11:54:16 +0300

    Task Done

---
+++ SanaC Sharp02/QuadraticEquation/Program.cs
@@ -0,0 +1,26 @@
+﻿int a, b, c;
+Console.WriteLine("Enter 3 digits:");
+Console.Write("A = ");
+a = int.Parse(Console.ReadLine());
+Console.Write("B = ");
+b = int.Parse(Console.ReadLine());
+Console.Write("C = ");
+c = int.Parse(Console.ReadLine());
+int d;
+d = b * b - 4 * a * c;
+double x1, x2;
+if (d > 0)
+{
+    x1 = -b + Math.Sqrt(d) / (2 * a);
+    x2 = -b - Math.Sqrt(d) / (2 * a);
+    Console.WriteLine($"x1 = {x1}, x2 = {x2}");
+}
+if (d == 0)
+{
+    x1 = -b / (2 * a);
+    Console.WriteLine($"x1 = {x1}");
+}
+if (d < 0)
+{
+    Console.WriteLine("Error! D < 0");
+}
---
+++ SanaC Sharp02/While1/Program.cs
@@ -0,0 +1,17 @@
+﻿int n;
+double a = 0;
+double b = 0;
+double c = 0;
+int fact = 1;
+Console.Write("Enter n = ");
+n = int.Parse(Console.ReadLine());
+for (int i  = 1; i <= n; i++)
+{
+    a += Math.Pow(-1, n) / (2.0 * n + 1);
+    b += (1 + (1.0 / (n * n)));
+    fact *= i;
+    c += fact;
+}
+Console.WriteLine($"a = {a}");
+Console.WriteLine($"b = {b}");
+Console.WriteLine($"c = {c}");
