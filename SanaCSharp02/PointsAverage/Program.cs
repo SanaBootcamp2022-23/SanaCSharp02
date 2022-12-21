d1cb9632253d844c52c5078b773b2b0d0b059edf
Taras Predchuk <ipzm222_ptv@student.ztu.edu.ua>
2022-12-21 16:47:28 +0200

    PointsAverage

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter first mark ");
+int FirstMark = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second mark ");
+int SecondMark = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter third mark ");
+int ThirdMark = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter fourth mark ");
+int FourthMark = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter fifth mark ");
+int FifthMark = int.Parse(Console.ReadLine());
+
+int summ = FirstMark+ SecondMark + ThirdMark + FourthMark + FifthMark;
+
+int avarage = summ / 5;
+
+if (avarage >= 4)
+    Console.WriteLine($"Student avarage mark is {avarage} admission granted");
+else
+    Console.WriteLine($"Student avarage mark is {avarage} access is denied");
