a1d8968e0bfe1d8a741bbc703341453e9db08cb3
tabbbyzzxc <ipz221_mms@student.ztu.edu.ua>
2022-12-21 12:08:16 +0100

    tasks done

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Transactions;
+
+namespace PointsAverage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            int total = 0;
+            Console.WriteLine("Введіть ваші оцінки: ");
+            for (int i = 0; i < 5; i++)
+            {
+                int mark = int.Parse(Console.ReadLine());
+                total += mark;
+            }
+            total /= 5;
+            if(total > 4)
+            {
+                Console.WriteLine("Допущено");
+            }
+            else
+            {
+                Console.WriteLine("Не допущено");
+            }
+        }
+    }
+}
