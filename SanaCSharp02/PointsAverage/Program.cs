a549b9a4970851a229d1e03499af5abe8975724f
Andrii Babushko <andriibabushko@gmail.com>
2022-12-21 15:59:18 +0200

    Complete Points Average task

--- SanaCSharp02/PointsAverage/Program.cs
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int PointsCount = 5;
+double Point, PointsSum = 0;
+
+for (int i = 0; i < PointsCount; i++)
+{
+    do
+    {
+        Console.Write($"{i + 1}) Enter student point: ");
+        if (double.TryParse(Console.ReadLine(), out Point) && Point >= 1 && Point <= 5)
+            break;
+        else
+        {
+            Console.WriteLine("Something went wrong!");
+            Console.Write("Repeat entering student point: ");
+        }
+    } while (double.TryParse(Console.ReadLine(), out Point) && !(Point >= 1 && Point <= 5)) ;
+
+    PointsSum += Point;
+}
+
+double PointsAverage = PointsSum / PointsCount;
+
+Console.WriteLine($"Points sum: {PointsSum}; Points average: {PointsAverage}");
+
+if (PointsAverage >= 4)
+    Console.WriteLine("The stundet is admitted to the exam!");
+else
+    Console.WriteLine("The stundet is not admitted to the exam!");
