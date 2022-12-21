0ba184c3388e6950eeea1f771652da29711182f9
Ivan Plias <sana22_pii@student.ztu.edu.ua>
2022-12-21 20:45:41 +0200

    PointsAverage ready

--- SanaCSharp02/PointsAverage/Program.cs
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] studentMarks = new int[5];
+int sum = 0;
+for (int i = 0; i < studentMarks.Length; i++)
+{
+    do
+        Console.WriteLine($"Input Mark {i + 1} = ");
+    while (!(int.TryParse(Console.ReadLine(), out studentMarks[i]) && studentMarks[i] > 0 && studentMarks[i] <= 5));
+    sum += studentMarks[i];
+}
+if (sum / 5 >= 4)
+    Console.WriteLine("The student is admitted to the exam");
+else
+    Console.WriteLine("The student is not admitted to the exam");
