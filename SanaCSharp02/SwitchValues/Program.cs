a754f4efcab5fa7c671d4bf3df2f2c197551ab1f
Ivan Savchuk <ipzm222_sio@student.ztu.edu.ua>
2022-12-21 23:12:37 +0200

    Task 3 (SwitchValues) done

---
+++ SanaCSharp02/SwitchValues/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+int value = 0;
+Console.WriteLine("Введіть ціле число в діапазоні від 0 до 9:");
+
+value = Convert.ToInt32(Console.ReadLine());
+
+switch (value)
+{
+    case 0: Console.WriteLine("нуль");
+        break;
+
+    case 1: Console.WriteLine("один");
+        break;
+
+    case 2:
+        Console.WriteLine("два");
+        break;
+
+    case 3:
+        Console.WriteLine("три");
+        break;
+
+    case 4:
+        Console.WriteLine("чотири");
+        break;
+
+    case 5:
+        Console.WriteLine("п'ять");
+        break;
+
+    case 6:
+        Console.WriteLine("шість");
+        break;
+
+    case 7:
+        Console.WriteLine("сім");
+        break;
+
+    case 8:
+        Console.WriteLine("вісім");
+        break;
+
+    case 9:
+        Console.WriteLine("дев'ять");
+        break;
+
+    default: Console.WriteLine("Введене число поза вказаним діапазоном!");
+        break;
+    
+}
