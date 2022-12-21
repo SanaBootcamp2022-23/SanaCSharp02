4b4059f20472ecfe04a9a41ddf60ec132bd9d3b6
Andrii <latasandrej@gmail.com>
2022-12-21 12:52:56 +0200

    Created PointsAvarege

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введіть 5 оцінок студента:");
+double point = 0;
+point += double.Parse(Console.ReadLine());
+point += double.Parse(Console.ReadLine());
+point += double.Parse(Console.ReadLine());
+point += double.Parse(Console.ReadLine());
+point += double.Parse(Console.ReadLine());
+double avgPoint = point / 5;
+Console.WriteLine("Середній бал студента: " + avgPoint);
+if (avgPoint >= 4)
+{
+    Console.WriteLine("Студент допущений до екзамену");
+}
+if (avgPoint < 4)
+{
+    Console.WriteLine("Студент не допущений до екзамену");
+}
+
