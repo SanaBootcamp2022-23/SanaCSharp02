f2d1e275be458e28921c7837b997fdb116e3292b
Ivan Plias <sana22_pii@student.ztu.edu.ua>
2022-12-21 20:34:44 +0200

    QuadraticEquation ready

--- SanaCSharp02/QuadraticEquation/Program.cs
+++ SanaCSharp02/QuadraticEquation/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double a, b, c;
+do
+    Console.WriteLine("Input a = ");
+while (!double.TryParse(Console.ReadLine(), out a));
+do
+    Console.WriteLine("Input b= ");
+while (!double.TryParse(Console.ReadLine(), out b));
+do
+    Console.WriteLine("Input c= ");
+while (!double.TryParse(Console.ReadLine(), out c));
+double D=(Math.Pow(b,2)- 4*a*c);
+if (D>0)
+{
+    double x1, x2;
+    x1=(-b+Math.Sqrt(D))/(2*a);
+    x2=(-b-Math.Sqrt(D))/(2*a);
+    Console.WriteLine($"x1= {x1}\nx2= {x2}");
+}
+else 
+    if(D==0)
+{
+    double x;
+    x = -b / (2 * a);
+    Console.WriteLine($"x= {x}\n");
+}
+else
+{
+    Console.WriteLine("There are no real roots when D < 0.");
+}
