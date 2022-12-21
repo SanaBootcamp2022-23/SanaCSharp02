8013bb7abc4d4145f680529d6c25b003a26a1164
Микита Шевцов <ipz222_shms@student.ztu.edu.ua>
2022-12-21 13:48:49 +0200

    Завдання 2 : PointsAverage

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,14 @@
+﻿List<int> Grades = new();
+int GradeSum = 0;
+
+Console.WriteLine("Введіть оцінки:");
+for (int i=0;i<5;i++)
+{
+    Grades.Add(int.Parse(Console.ReadLine()));
+    GradeSum += Grades[i];
+}
+
+if (GradeSum / Grades.Count >= 4)
+    Console.WriteLine("Студента допущено до екзамену.");
+else
+    Console.WriteLine("Студента НЕ допущено до екзамену!");
