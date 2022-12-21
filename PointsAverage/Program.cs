5a987eb6073bf5967e58368078122195358a0ca5
Vyacheslav Smolianiuk <sana22_svv@student.ztu.edu.ua>
2022-12-21 23:49:04 +0200

    all task done

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,19 @@
+﻿double[] count=new double[10];
+for(int i=0; i < 5; i++)
+{
+    count[i] = double.Parse(Console.ReadLine());
+}
+double summ = 0;
+for (int i = 0; i < 5; i++)
+{
+    summ+=count[i];
+}
+if (summ / 5 < 4)
+{
+    Console.WriteLine($"Ти маєш не допуск до екзмену!!!\nTвоя оцiнка \"{summ/5.0}\"");
+
+}
+else
+{
+    Console.WriteLine("Ваш середнiй бал 4 i бiльше, маєш допуск до екзамену))");
+}
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,31 @@
+﻿
+double a, b, c, d, x1, x2;
+
+Console.Write("Будь-ласка введiть число a: ");
+a = double.Parse(Console.ReadLine());
+Console.Write($"було введено а= {a}\n");
+Console.Write("Будь-ласка введiть число b: ");
+b = double.Parse(Console.ReadLine());
+Console.Write($"було введено b= {b}\n");
+Console.Write("Будь-ласка введiть число c: ");
+c = double.Parse(Console.ReadLine());
+Console.Write($"було введено c= {c}\n");
+
+d = b * b - 4 * a * b;
+if (d > 0)
+{
+    x1 = (-b - Math.Sqrt(d)) / (2 * a);
+    x2 = (-b + Math.Sqrt(d)) / (2 * a);
+    Console.Write($"Перший корiнь х1={x1}\n");
+    Console.Write($"другий корiнь х2={x2}\n");
+}
+if (d == 0)
+{
+    x1 = -b / (2 * a);
+    Console.Write($"Єдиний корiнь х={x1}\n");
+}
+if(d<0)
+{
+    Console.WriteLine("На жаль розв'язкiв тут немаємо ....");
+
+}
