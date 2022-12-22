b0e2c9788c7eb4e9145b27473f0e7e2e4a8d74be
Kuprienko Anatolii <ipz222_kaa@student.ztu.edu.ua>
2022-12-22 20:35:52 +0200

    SanaCSharp02

---
+++ SwitchValues/Program.cs
@@ -0,0 +1,29 @@
+﻿int value;
+do
+{
+    Console.Write("Enter value: ");
+    value = int.Parse(Console.ReadLine());
+} while (value < 0 || value > 9);
+switch(value)
+{
+    case 0: 
+        Console.Write("Нуль"); break;
+    case 1:
+        Console.Write("Один"); break;
+    case 2:
+        Console.Write("Два"); break;
+    case 3: 
+        Console.Write("Три"); break;
+    case 4:
+        Console.Write("Чотири"); break;
+    case 5:
+        Console.Write("П'ять"); break;
+    case 6:
+        Console.Write("Шість"); break;
+    case 7:
+        Console.Write("Сім"); break;
+    case 8:
+        Console.Write("Вісім"); break;
+    case 9:
+        Console.Write("Дев'ять"); break;
+}
---
+++ While1/Program.cs
@@ -0,0 +1,16 @@
+﻿sbyte n;
+double a = 0, b = 0, c = 0, fact = 1;
+Console.Write("n = ");
+n = sbyte.Parse(Console.ReadLine());
+for(int i = 1; i <= n; i++)
+    a += Math.Pow(-1, i) / (2 * n + 1);
+Console.WriteLine($"a = {a}");
+for (int i = 1; i <= n; i++)
+    b += 1 + 1 / Math.Pow(i, 2);
+Console.WriteLine($"b = {b}");
+for(int i = 1; i <= n; i++)
+{
+    fact *= i;
+    c += fact;
+}
+Console.Write($"c = {c}");
