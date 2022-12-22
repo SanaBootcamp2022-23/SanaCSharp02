19a200d5797d7f93a8a918ae71a6b3f78fa9c325
Andrii Nemerovskii <andreynemerovskiy@gmail.com>
2022-12-22 23:29:26 +0200

    Complete SwitchValues

--- SanaCSharp02/PointsAverage/Program.cs
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -2,7 +2,7 @@
 Console.OutputEncoding = Encoding.Unicode;
 Console.InputEncoding = Encoding.Unicode;
 
-double point1, point2, point3, point4, point5, avarage;
+double point1, point2, point3, point4, point5, avarege;
 Console.WriteLine("Введіть 5 оцінок студента:");
 point1 = double.Parse(Console.ReadLine());
 point2 = double.Parse(Console.ReadLine());
@@ -10,9 +10,9 @@ point3 = double.Parse(Console.ReadLine());
 point4 = double.Parse(Console.ReadLine());
 point5 = double.Parse(Console.ReadLine());
 
-avarage = (point1 + point2 + point3 + point4 + point5) / 5.0;
-Console.WriteLine($"Серднє арифметичне = {avarage}");
-if (avarage > 4.0)
+avarege = (point1 + point2 + point3 + point4 + point5) / 5.0;
+Console.WriteLine($"Серднє арифметичне = {avarege}");
+if (avarege > 4.0)
     Console.WriteLine("Студент допущений до екзамену");
 else
     Console.WriteLine("Студент не допущений до екзамену");
