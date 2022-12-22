11d65f6bbd9d6b792def77e0f1349599a684db64
Andrii Nemerovskii <andreynemerovskiy@gmail.com>
2022-12-23 00:25:40 +0200

    Complete sanacsharp02

--- SanaCSharp02/While1/Program.cs
+++ SanaCSharp02/While1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+double a = 0, b = 0, c = 1, n, factorial = 1;
+Console.Write("Введіть число: ");   n = double.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    a += Math.Pow((-1), i) / (2 * i + 1);
+}
+for (int i = 1; i <= n; i++)
+{
+    b += 1 + (1 / Math.Pow(i, 2));
+}
+for (int i = 1; i <= n; i++)
+{
+    c *= i;
+}
+Console.WriteLine($"a = {a}");
+Console.WriteLine($"b = {b}");
+Console.WriteLine($"c = {c}");
