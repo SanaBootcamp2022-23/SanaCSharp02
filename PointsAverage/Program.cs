9876bcc39d7469009b55d9626a998300838cab42
Shabotinskyi Dmytro <vt221_shdyu@student.ztu.edu.ua>
2022-12-23 00:12:27 +0200

    Completed work

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter 5 ratings");
+        int a, average, summa = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write($"{i + 1} rating: ");
+            a = int.Parse(Console.ReadLine());
+            if (a > 0 && a <= 5)
+                summa += a;
+            else
+            {
+                Console.WriteLine("Error! The number is out of order");
+                return;
+            }
+        }
+        average = summa / 5;
+        Console.WriteLine($"Average mark {average}");
+        if (average >= 4)
+            Console.WriteLine("The student has a permit");
+        else
+            Console.WriteLine("Student does not have access");
+    }
+}
---
+++ QuadraticEquation/QuadraticEquation/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program {
+    private static void Main(string[] args){
+        int a, b, c;
+        double D, x1, x2,x;
+        Console.Write("Press A: ");
+        a = int.Parse(Console.ReadLine());
+        Console.Write("Press B: ");
+        b = int.Parse(Console.ReadLine());
+        Console.Write("Press C: ");
+        c = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Quadratic Equation: {a}x^2+{b}x+{c}=0");
+        D = b * b - (4 * a * c);
+        Console.WriteLine($"D = {D}");
+        if (D == 0){
+            x = -b / (2 * a);
+            Console.WriteLine($"{x}");
+        }
+        if (D > 0){
+            x1 = (-b + Math.Sqrt(D)) / (2 * a);
+            x2 = (-b - Math.Sqrt(D)) / (2 * a);
+            Console.WriteLine($"{x1}");
+            Console.WriteLine($"{x2}");
+        }
+        if (D < 0){
+            Console.WriteLine("Error! D < 0");
+        }
+    }
+}
+
+
