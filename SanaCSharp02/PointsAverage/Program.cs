e7dd677611cd1f88b2978f5c33b568a13319d0a3
tetianakostiuk <ipz213_kto@student.ztu.edu.ua>
2022-12-23 22:39:30 +0200

    SanaCSharp02

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,7 @@
+﻿int value = 0;
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"Enter value {i}:");
+    value += int.Parse((Console.ReadLine()));
+}
+Console.WriteLine($"Average is: {value / 5}");
---
+++ SanaCSharp02/SwitchValues/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine("enter n [0,9]:");
+int n = int.Parse(Console.ReadLine());
+if (n <= 9 && n >= 0)
+{
+    switch (n)
+    {
+        case 0:
+            Console.WriteLine("zero");
+            break;
+        case 1:
+            Console.WriteLine("one");
+            break;
+        case 2:
+            Console.WriteLine("two");
+            break;
+        case 3:
+            Console.WriteLine("three");
+            break;
+        case 4:
+            Console.WriteLine("four");
+            break;
+        case 5:
+            Console.WriteLine("five");
+            break;
+        case 6:
+            Console.WriteLine("six");
+            break;
+        case 7:
+            Console.WriteLine("seven");
+            break;
+        case 8:
+            Console.WriteLine("eight");
+            break;
+        case 9:
+            Console.WriteLine("nine");
+            break;
+    }
+
+
+}
+else
+{
+    Console.WriteLine("n is out of range [0,9]");
+}
