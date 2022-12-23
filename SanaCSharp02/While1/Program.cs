a58a07a522d7a169fe4ea6445e2b7fe9359514a1
Olha36 <olhakucheruk36@gmail.com>
2022-12-23 12:54:31 +0100

    tasks are done)

--- SanaCSharp02/While1/Program.cs
+++ SanaCSharp02/While1/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int n;
+Console.Write("n = ");
+n = int.Parse(Console.ReadLine());
+double a, b, c;
+double factorial = 1;
+Console.Write("a = ");
+a = int.Parse(Console.ReadLine());
+Console.Write("b = ");
+b = int.Parse(Console.ReadLine());
+Console.Write("c = ");
+c = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    a += Math.Pow(-1, i) / (2 * i + 1);
+    b += 1 + 1 / Math.Pow(i, 2);
+    factorial*= i;
+    c += 1 / factorial;
+}
