3424587856f037eab828a27723d32e519feb4161
OleksiyBubenko <ipz202_bov@student.ztu.edu.ua>
2022-12-23 21:57:33 +0200

    Done task3

--- SanaCSharp02/Math/Math.cs
+++ SanaCSharp02/Math/Math.cs
@@ -65,5 +65,23 @@ namespace MathLibrary
             var average = sum / numbers.Length;
             return average;
         }
+
+        public static string GetTextNumber(int number)
+        {
+            return number switch
+            {
+                1 => "one",
+                2 => "two",
+                3 => "three",
+                4 => "four",
+                5 => "five",
+                6 => "six",
+                7 => "seven",
+                8 => "eight",
+                9 => "nine",
+                0 => "zero",
+                _ => ""
+            };
+        }
     }
 }
---
+++ SanaCSharp02/SwitchValues/Program.cs
@@ -0,0 +1,27 @@
+﻿using Helpers;
+
+namespace SwitchValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The program \"SwitchValues\"");
+
+            while (true)
+            {
+                Console.WriteLine("\nPlease input integer number in the range [0-9].");
+                var number = ConsoleReader.ReadInt("number", 0, 9);
+
+                var numberText = MathLibrary.Math.GetTextNumber(number);
+                Console.WriteLine($"\nResult: {numberText}");
+
+                Console.WriteLine("\nContinue? 1 - Yes; 0 - No");
+                var continueNumber = ConsoleReader.ReadInt("continue", 0, 1);
+
+                if (continueNumber != 1)
+                    break;
+            }
+        }
+    }
+}
