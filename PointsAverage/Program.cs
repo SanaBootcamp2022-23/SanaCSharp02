2570328554e12ba99c0713ca19efdf4e73c6b56f
Aleksandr Marchenko <sana22_moa@student.ztu.edu.ua>
2022-12-23 13:25:46 +0200

    SanaCSharp02

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+int count;
+Console.Write("Введите количество полученных оценок = ");
+count = int.Parse(Console.ReadLine());
+double summa = 0;
+int[] array = new int[count];
+for (int i = 0; i < count; i++)
+{
+    Console.Write($"Введите текущую оценку от 1 до 5 баллов {i + 1}-  ");
+    array[i] = int.Parse(Console.ReadLine());
+    while (array[i] > 5)
+    {
+        Console.WriteLine($"Оценка введена не верно");
+        Console.Write($"Введите текущую оценку от 1 до 5 баллов {i + 1}-  ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    while (array[i] <= 0)
+    {
+        Console.WriteLine($"Оценка введена не верно");
+        Console.Write($"Введите текущую оценку от 1 до 5 баллов {i + 1}-  ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    summa += array[i];
+}
+double avarage = summa / count;
+Console.WriteLine($"avarage = {avarage}");
+if (avarage >= 4)
+    Console.Write("К экзамену допущен");
+else
+    Console.Write("К экзамену не допущен");
---
+++ While1/Program.cs
@@ -0,0 +1,17 @@
+﻿int N;
+double resultA = 0;
+double resultB = 0;
+double resultC = 0;
+double factorial = 1;
+Console.Write("n = ");
+N = int.Parse(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    resultA += Math.Pow(-1.0, i) / (2 * i + 1);
+    resultB += 1 + 1 / Math.Pow(i, 2);
+    factorial *= i;
+    resultC += factorial;
+}
+Console.WriteLine($"A = {resultA}");
+Console.WriteLine($"B = {resultB}");
+Console.WriteLine($"C = {resultC}");
