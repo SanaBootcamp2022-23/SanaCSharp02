51c932c31cfb3dd3879e66e0ed46e843a4603111
Andrii Nemerovskii <andreynemerovskiy@gmail.com>
2022-12-23 23:57:27 +0200

    Chenged While1

--- SanaCSharp02/While1/Program.cs
+++ SanaCSharp02/While1/Program.cs
@@ -2,7 +2,7 @@
 Console.OutputEncoding = Encoding.Unicode;
 Console.InputEncoding = Encoding.Unicode;
 
-double a = 0, b = 0, c = 1, n, factorial = 1;
+double a = 0, b = 0, c = 0, n, factorial = 1;
 Console.Write("Введіть число: ");   n = double.Parse(Console.ReadLine());
 for (int i = 1; i <= n; i++)
 {
@@ -14,7 +14,8 @@ for (int i = 1; i <= n; i++)
 }
 for (int i = 1; i <= n; i++)
 {
-    c *= i;
+    factorial *= i;
+    c += factorial;
 }
 Console.WriteLine($"a = {a}");
 Console.WriteLine($"b = {b}");
