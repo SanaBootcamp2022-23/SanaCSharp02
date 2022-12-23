3850dc5e680dae1850d37df7ea22ccb4b5e20f2c
Olha36 <olhakucheruk36@gmail.com>
2022-12-23 12:12:14 +0100

    tasks are done

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int total = 5;
+int average = 0;
+
+Console.Write("Введіть оцінки: ");
+
+
+for (int i = 1; i < total; i++)
+{
+    total += int.Parse(Console.ReadLine());
+}
+
+average = total / 5;
+
+if (average >= 4)
+{
+    Console.WriteLine("Студента допущено до екзамену.");
+}
+else
+{
+    Console.WriteLine("Студента не допущено до екзамену.");
+}
