d186ef4bfeea035786a19d2b6ee9624a4f6c68dd
Yaroslav <ipz212_tsyav@student.ztu.edu.ua>
2022-12-25 00:36:48 +0200

    SanaCSharp02 push

---
+++ SwitchValues/Program.cs
@@ -0,0 +1,56 @@
+﻿Console.Write("Введіть число в діапазоні[0-9]");
+int number = int.Parse(Console.ReadLine());
+switch(number)
+{
+    case 0:
+        {
+            Console.WriteLine("Нуль");
+            break;
+        }
+    case 1:
+        {
+            Console.WriteLine("Один");
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine("Два");
+            break;
+        }
+    case 3:
+        {
+            Console.WriteLine("Три");
+            break;
+        }
+    case 4:
+        {
+            Console.WriteLine("Чотири");
+            break;
+        }
+    case 5:
+        {
+            Console.WriteLine("П'ять");
+            break;
+        }
+    case 6:
+        {
+            Console.WriteLine("Шість");
+            break;
+        }
+    case 7:
+        {
+            Console.WriteLine("Сім");
+            break;
+        }
+    case 8:
+        {
+            Console.WriteLine("Вісім");
+            break;
+        }
+    case 9:
+        {
+            Console.WriteLine("Дев'ять");
+            break;
+        }
+
+}
