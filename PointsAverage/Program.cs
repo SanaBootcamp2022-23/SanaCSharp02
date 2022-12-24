599777138d867531c922f5434e314f109cbc6cb0
Yaroslav Mykhalchuk <ipzm222_myaa@student.ztu.edu.ua>
2022-12-24 17:19:57 +0200

    Points Average done

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,40 @@
+﻿
+int point;
+double sumPoints = 0;
+
+for (int i = 1; i <= 5; i++)
+{
+    CheckAndWriteNumber(out point, i);
+    sumPoints += point;
+}
+
+FindOutAdmittion();
+
+
+void CheckAndWriteNumber(out int point, int i)
+{
+    bool isRightNumber;
+    do
+    {
+        Console.Write($"Input {i} point: ");
+        isRightNumber = int.TryParse(Console.ReadLine(), out point);
+        if (point > 5 || point < 1)
+            isRightNumber = false;
+        if (!isRightNumber)
+        {
+            Console.Write("ERROR!!!\n");
+        }
+    } while (!isRightNumber);
+}
+
+void FindOutAdmittion()
+{
+    if ((sumPoints / 5) >= 4)
+    {
+        Console.WriteLine("This student is admitted to exam.");
+    }
+    else
+    {
+        Console.WriteLine("This student is not admitted to exam.");
+    }
+}
