9f61d044f53cbdb586cb9bd0f9fad970801a5cae
Blokha Yaroslav <ipz213_byas@student.ztu.edu.ua>
2022-12-24 22:12:44 +0200

    SanaCSharp02

---
+++ SanaCSharp02/While1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace While1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть число:");
+            int n = int.Parse(Console.ReadLine());
+            double a = 0, b = 0, c = 0, f = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                a += Math.Pow(-1, i) / (2 * i + 1);
+                b += 1 + 1 / Math.Pow(i, 2);
+                f *= i;
+                c += f;  
+            }
+            Console.WriteLine($"a = {a}\nb = {b}\nc = {c}");
+        }
+    }
+}
