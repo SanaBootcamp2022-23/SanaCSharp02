055d386d5b2b3d21cde1459ec1b14c53b647e6a7
Danyil Tuhaienko <kpz21_tdo@student.ztu.edu.ua>
2022-12-24 16:04:26 +0100

    Solution created, task 1 - QuadraticEquation

---
+++ SanaCSharp02/QuadraticEquation/Program.cs
@@ -0,0 +1,44 @@
+﻿while (true)
+{
+    double a, b, c;
+
+    Console.Write("Enter a -> ");
+    if (!double.TryParse(Console.ReadLine(), out a))
+    {
+        Console.WriteLine("Incorrect value");
+        continue;
+    }
+    Console.Write("Enter b -> ");
+    if (!double.TryParse(Console.ReadLine(), out b))
+    {
+        Console.WriteLine("Incorrect value");
+        continue;
+    }
+    Console.Write("Enter c -> ");
+    if (!double.TryParse(Console.ReadLine(), out c))
+    {
+        Console.WriteLine("Incorrect value");
+        continue;
+    }
+
+    double discriminant = b * b - 4 * a * c;
+    Console.WriteLine("discriminant = " + discriminant);
+
+    if (discriminant > 0)
+    {
+        double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+        double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+        Console.WriteLine("x1 = " + x1);
+        Console.WriteLine("x2 = " + x2);
+    }
+    else if (discriminant == 0)
+    {
+        double x1 = -b / (2 * a);
+        Console.WriteLine("x1 = " + x1);
+    }
+    else
+    {
+        Console.WriteLine("Discriminant less than zero");
+    }
+    Console.WriteLine();
+}
