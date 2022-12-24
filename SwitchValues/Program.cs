9919fe85532ab4f028dff81154a4b803e69b3dbf
Viktor Opanasyuk <ipz223-ovs@student.ztu.edu.ua>
2022-12-24 19:27:19 +0200

    Completed Task

---
+++ SwitchValues/Program.cs
@@ -0,0 +1,41 @@
+﻿ushort value=0;
+while (true) { 
+Console.Write("Enter the value in range [0;9] or [10] for exit: ");
+if (!ushort.TryParse(Console.ReadLine(), out value)) return;
+    switch (value)
+    {
+        case 0:
+            Console.WriteLine("Ноль");
+            break;
+        case 1:
+            Console.WriteLine("Один");
+            break;
+        case 2:
+            Console.WriteLine("Два");
+            break;
+        case 3:
+            Console.WriteLine("Три");
+            break;
+        case 4:
+            Console.WriteLine("Чотири");
+            break;
+        case 5:
+            Console.WriteLine("П'ять");
+            break;
+        case 6:
+            Console.WriteLine("Шість");
+            break;
+        case 7:
+            Console.WriteLine("Сім");
+            break;
+        case 8:
+            Console.WriteLine("Вісім");
+            break;
+        case 9:
+            Console.WriteLine("Дев'ять");
+            break;
+        default:
+            Console.WriteLine("Ви вийшли з програми");
+            return;
+    }
+}
---
+++ While1/Program.cs
@@ -0,0 +1,17 @@
+﻿uint valueN = 0;
+
+Console.Write("Enter the value: ");
+if (!uint.TryParse(Console.ReadLine(), out valueN)) return;
+
+double a = 0, b = 0, c = 0 ;
+int factorial(int num)
+{
+    return num-1<=0? num :num * factorial(num-1); 
+}
+for (int i = 1; i <= valueN; i++)
+{
+    a += Math.Pow(-1, i) / (2 * i + 1);
+    b += 1 + 1 / (i * i);
+    c += factorial(i);
+}
+Console.WriteLine($"a = { a}\nb = {b}\nc = {c}");
