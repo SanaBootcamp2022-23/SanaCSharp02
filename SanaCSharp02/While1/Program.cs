5e3ca38ec1caa16622a09311667c5dc8dc2f2c5f
Kateryna Hladka <57210621+k-hladka@users.noreply.github.com>
2022-12-24 20:59:55 +0200

    A program that takes a number and evaluates three expressions

---
+++ SanaCSharp02/While1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+Console.Write("Введіть натуральне число n: ");
+int n = int.Parse(Console.ReadLine());
+if(n <= 0)
+{
+    Console.Write("Ви ввели не натуральне число!");
+    return;
+}
+double a = 0, b = 0, c = 0, factorial = 1;
+for(int i=1; i<=n; i++)
+{
+    a += Math.Pow(-1, i) / (2 * i + 1);
+    b += 1 + 1 / Math.Pow(i, 2);
+    factorial *= i; 
+    c += factorial;
+}
+Console.Write($"a = {a}\nb = {b}\nc = {c}");
