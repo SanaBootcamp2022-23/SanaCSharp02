1f94a2b4274ae513e660e10a385bd7a250906ac3
Kateryna Hladka <57210621+k-hladka@users.noreply.github.com>
2022-12-24 20:25:50 +0200

    A program that accepts points and checks whether a student is admitted to the exam

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+int pointsSum=0;
+string str;
+for (int i = 1; i <= 5; i++)
+{
+    switch (i)
+    {
+        case 1: str = "шу"; break;
+        case 2: str = "гу"; break;
+        case 3: str = "тю"; break;
+        default: str = "ту";break;
+
+    }
+    Console.Write($"Введіть {i}-{str} оцінку: ");
+    pointsSum += int.Parse(Console.ReadLine());
+}
+int avarage = pointsSum / 5;
+string result = (avarage >= 4) ? "Студент допущений " :"Студент не допущений";
+Console.Write(result);
