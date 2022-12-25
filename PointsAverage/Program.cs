79f4d457a4e6ebe675250713b74e5615f470ca9f
sana22mav <sana22_mav@student.ztu.edu.ua>
2022-12-26 01:50:48 +0200

    Done

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Калькулятор середнього балу");
+Console.WriteLine("Введіть 5 оцінок студента:");
+
+double score1 = Convert.ToDouble(Console.ReadLine());
+double score2 = Convert.ToDouble(Console.ReadLine());
+double score3 = Convert.ToDouble(Console.ReadLine());
+double score4 = Convert.ToDouble(Console.ReadLine());
+double score5 = Convert.ToDouble(Console.ReadLine());
+
+double averageScore = (score1 + score2 + score3 + score4 + score5) / 5;
+
+if (averageScore >= 4)
+{
+    Console.WriteLine("Студенту дозволено брати участь у екзамені.");
+}
+else
+{
+    Console.WriteLine("Студенту не дозволено брати участь у екзамені.");
+}
+Console.ReadLine();
---
+++ While1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("While1");
+Console.Write("Введіть натуральне число n: ");
+
+int n = int.Parse(Console.ReadLine());
+
+double a = 0;
+double b = 0;
+double c = 0;
+int factorial = 1;
+
+int i = 1;
+while (i <= n)
+{
+    a += Math.Pow(-1, i) / (2 * i + 1);
+    b += 1 + (1 / Math.Pow(i, 2));
+    factorial *= i;
+    c += factorial;
+    i++;
+}
+
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+Console.WriteLine("c = " + c);
+Console.ReadLine();
