07d8217e0eff962d55ff156cbbd668d67e30deda
Valerii Piaskivskiy <sana22_pvo1@student.ztu.edu.ua>
2022-12-25 17:18:56 +0200

    QuadraticEquation

---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,33 @@
+﻿double a, b, c;
+double x1, x2;
+
+
+Console.Write("Input number a = ");
+a = double.Parse(Console.ReadLine());
+
+Console.Write("Input number b = ");
+b = double.Parse(Console.ReadLine());
+
+Console.Write("Input number c = ");
+c = double.Parse(Console.ReadLine());
+
+var discriminant = Math.Pow(b, 2) - 4 * a * c;
+
+if (discriminant < 0)
+{
+    Console.WriteLine("This quadratic equation has no roots");
+}
+else
+{
+    if (discriminant == 0)
+    {
+        x1 = -b / (2 * a);
+        x2 = x1;
+    }
+    else
+    {
+        x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+        x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+    }
+    Console.WriteLine($"\n x1 = {x1}; \n x2 = {x2}");
+}
