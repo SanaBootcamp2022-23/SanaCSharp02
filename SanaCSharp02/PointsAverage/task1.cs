74af4a32f8debf84ccf0828e243bc4a2cee5a13f
DanyloDziubchuk <vt211_ddr@student.ztu.edu.ua>
2022-12-25 11:30:32 +0200

    SanaCSharp02 is done

---
+++ SanaCSharp02/PointsAverage/task1.cs
@@ -0,0 +1,26 @@
+﻿double mark;
+int count = 0;
+double sum = 0;
+
+while (count < 5)
+{
+    Console.WriteLine("Введiть оцiнку: ");
+    while (!Double.TryParse(Console.ReadLine(), out mark))
+    {
+        Console.WriteLine("Введено невiрне значення для оцінки!\n Спробуйте ще раз!");
+    }
+
+    sum += mark;
+    count++;
+}
+
+double avgMark = sum / count;
+Console.WriteLine($"Середнiй бал студента: {avgMark}");
+if(avgMark >= 4)
+{
+    Console.WriteLine("Студент допущений до екзамену!");
+}
+else
+{
+    Console.WriteLine("Студент не допущений до екзамену!");
+}
---
+++ SanaCSharp02/While1/task4.cs
@@ -0,0 +1,50 @@
+﻿int n,temp_n;
+
+Console.WriteLine("Введiть число n:");
+while (!Int32.TryParse(Console.ReadLine(), out n))
+{
+    Console.WriteLine("Введено невiрне значення для числа!\n Спробуйте ще раз!");
+}
+
+// a
+double a = 0;
+temp_n = n;
+while (temp_n >= 1)
+{
+    a += Math.Pow(-1, temp_n) / (2 * temp_n + 1);
+    temp_n--;
+}
+
+Console.WriteLine($"a = {a}");
+// a
+
+// b
+double b = 0;
+temp_n = n;
+while (temp_n >= 1)
+{
+    b += 1 + (1.0/Math.Pow(temp_n,2));
+    temp_n--;
+}
+
+Console.WriteLine($"b = {b}");
+// b
+
+// c
+double c = 0;
+temp_n = n;
+while (temp_n >= 1)
+{
+    int temp2_n = temp_n;
+    int fact = 1;
+    while (temp2_n >= 1)
+    {
+        fact *= temp2_n;
+        temp2_n--;
+    }
+    c += fact;
+    temp_n--;
+}
+
+Console.WriteLine($"c = {c}");
+// c
