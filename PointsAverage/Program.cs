dbe46821862cc549457e26da594e3adae98ecbaf
ipz222_tmo <ipz222_tmo@student.ztu.edu.ua>
2022-12-25 13:41:01 +0200

    Виконано практичну роботу №04.

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+int grade;
+int summa=0;
+double avg;
+Console.WriteLine("Введіть оцінки студента:");
+for (int i = 0; i < 5; i++) 
+{
+    grade = int.Parse(Console.ReadLine());
+    if (grade < 0) Console.WriteLine("Error: Grade can not be < 0");
+    summa += grade;
+}
+avg = summa / 5.0;
+if (avg < 4.0) Console.WriteLine($"Студент НЕ допущений до екзамену\nЙого/її оцінка - {avg}.");
+else Console.WriteLine($"Студент допущений до екзамену\nЙого/її оцінка - {avg}.");
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata.Ecma335;
+
+int a, b, c;
+int discriminant;
+double x1, x2;
+Console.Write("Input a: ");
+a = int.Parse(Console.ReadLine());
+Console.Write("Input b: ");
+b = int.Parse(Console.ReadLine());
+Console.Write("Input c: ");
+c = int.Parse(Console.ReadLine());
+discriminant = (b * b) - 4 * a * c;
+x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
+x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
+Console.WriteLine("\nResult:");
+if (discriminant > 0) Console.WriteLine($"Discriminant = {discriminant}\nx1 = {x1} \nx2 = {x2}");
+if (discriminant == 0) Console.WriteLine($"Discriminant = {discriminant}\nx = {x1}");
+else if (discriminant < 0) Console.WriteLine("Error: Discriminant < 0");
