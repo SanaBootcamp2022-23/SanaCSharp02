78665d62e2f5a4cb4023d751fb20d319fd22038a
Oleksandr Parkhomenko <ipzm222_pol@student.ztu.edu.ua>
2022-12-26 21:59:58 +0200

    Quadratic&PointsAverage is ready

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+double avrMark = 0;
+int sumMark = 0;
+Console.WriteLine("Please, enter grade mark from 1 to 5");
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine($"grade mark {i}");
+    int mark = enterData();
+    sumMark = sumMark + mark;
+}
+avrMark = sumMark / 5;
+if(avrMark >= 4)
+{
+    Console.WriteLine("You are allowed, welcome");
+}
+else
+{
+    Console.WriteLine("It`s a pity, but you`re not allowed");
+}
+
+int enterData()
+{
+    string consoleString = Console.ReadLine();
+    return int.Parse(consoleString);
+}
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.CompilerServices;
+
+double a,b,c,d,x1,x2;
+Console.WriteLine("Program for calculating square expressions");
+
+a = enterData("Please enter coefficient a");
+b = enterData("Please enter coefficient b");
+c = enterData("Please enter coefficient c");
+
+double discriminantCalculate(double a, double b, double c)
+{
+    return Math.Pow(b, 2) - 4 * a * c;
+}
+
+d = discriminantCalculate(a,b,c);
+
+if (d > 0)
+{
+    x1 = (-b + Math.Sqrt(d)) / 2 * a;
+    x2 = (-b - Math.Sqrt(d)) / 2 * a;
+    outResult("Equation has two roots, `cause discriminant is positive and bigger than 0. D = ",d);
+    outResult("x1 = ",x1);
+    outResult("x2 = ",x2);
+}
+else if (d == 0)
+{
+    x1 = (-b - Math.Sqrt(c)) / 2 * a;
+    outResult("Equation has one root, `cause discriminant equals 0. D = ", d);
+    outResult("x1 = x2 = ", x1);
+}
+else
+{
+    outResult("Equation has no roots,`cause discriminant less than 0. D = ", d);
+}
+
+void outResult(string s, double x)
+{
+    Console.WriteLine(s + x);
+}
+
+double enterData(String s)
+{
+    
+    Console.WriteLine(s);
+    string consoleString = Console.ReadLine();
+    return double.Parse(consoleString);
+}
