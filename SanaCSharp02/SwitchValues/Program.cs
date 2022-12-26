c9b215c0fdaf996f2311dbc33ba80226e4d8cdbe
Andrii vovk <sana22_vam@student.ztu.edu.ua>
2022-12-26 15:58:35 +0200

    3d task is done

---
+++ SanaCSharp02/SwitchValues/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+double number, c;
+
+Console.Write("Enter number from 0 to 9:");
+c = Convert.ToDouble(Console.ReadLine());
+switch (c)
+{
+    case 0:
+        Console.Write("zero");
+        break;
+    case 1:
+        Console.Write("one");
+        break;
+    case 2:
+        Console.Write("two");
+        break;
+    case 3:
+        Console.Write("three");
+        break;
+    case 4:
+        Console.Write("four");
+        break;
+    case 5:
+        Console.Write("five");
+        break;
+    case 6:
+        Console.Write("six");
+        break;
+    case 7:
+        Console.Write("seven");
+        break;
+    case 8:
+        Console.Write("eight");
+        break;
+    case 9:
+        Console.Write("nine");
+        break;
+    default:
+        Console.WriteLine("Entered wrong number");
+        break;
+
+
+
+}
