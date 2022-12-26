6171a64e2a7b242c6ed23e5e100fd7672a501ba6
Andrii vovk <sana22_vam@student.ztu.edu.ua>
2022-12-26 15:49:56 +0200

    2tasks are done

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+double c, K;
+double a = 0;
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Enter mark: ");
+    c = Convert.ToDouble(Console.ReadLine());
+    a = a + c;
+}
+Console.WriteLine($"sum of marks : {a}\n");
+
+K = a / 5;
+if (K < 4)
+{
+    Console.WriteLine("Not allowed for examination");
+}
+else
+{
+    Console.WriteLine("Allowed for examination");
+}
---
+++ SanaCSharp02/QuadraticEquation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+double a, b, c, x, x1, x2, D;
+
+Console.WriteLine("Enter number a: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter number b: ");
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter number c: ");
+c = Convert.ToDouble(Console.ReadLine());
+D = Math.Pow(b, 2) - 4 * a * c;
+if (D < 0)
+    {
+    Console.WriteLine("Discriminant < 0, that`s mean, equation have no colutions");
+    }
+else if (a > 0)
+    {
+        if (D == 0)
+        { 
+            x = -b / (2 * a);
+            Console.WriteLine($"X = {x}");
+        }
+        else if (D > 0)
+        {
+            x1 = (- b + Math.Sqrt(D) ) / (2 * a);
+            x2 = (- b - Math.Sqrt(D) ) / (2 * a);
+            Console.WriteLine($"X1 = {x1}\n");
+            Console.WriteLine($"X2 = {x2}\n");
+
+
+        }
+    }
+        else
+        {
+            Console.WriteLine("Without any solutions");
+        }
