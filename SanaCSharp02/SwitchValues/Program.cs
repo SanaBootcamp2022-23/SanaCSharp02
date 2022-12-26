8b2f4d10c4cfe3d25af76fefca72132abbce0af7
Віктор Мінайленко <ipz224_mvyu@student.ztu.edu.ua>
2022-12-27 00:33:37 +0200

    Created project SwitchValues

---
+++ SanaCSharp02/SwitchValues/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введіть число від 1 до 9:");
+
+int position = int.Parse(Console.ReadLine());
+
+switch (position)
+{
+    case 1:
+        Console.WriteLine("Один");
+        break;
+    case 2:
+        Console.WriteLine("Два");
+        break;
+    case 3:
+        Console.WriteLine("Три");
+        break;
+    case 4:
+        Console.WriteLine("Чотири");
+        break;
+    case 5:
+        Console.WriteLine("П'ять");
+        break;
+    case 6:
+        Console.WriteLine("Шість");
+        break;
+    case 7:
+        Console.WriteLine("Сім");
+        break;
+    case 8:
+        Console.WriteLine("Вісім");
+        break;
+    case 9:
+        Console.WriteLine("Дев'ять");
+        break;
+    default:
+        Console.WriteLine("Неправильний ввід");
+        break;
+}
