54d49293d78924c036b7639034018157e86d79bd
nikotsim <tsimbalyuk88@gmail.com>
2022-12-26 20:56:36 +0200

    assignment PointsAverage completed

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Середня оцiнка студента");
+
+double sum = 0;
+Console.WriteLine("Введiть 5 оцiнок студента: ");
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i + 1 + " оцiнка");
+    sum += double.Parse(Console.ReadLine());
+}
+
+if (sum/5 >= 4)
+{
+    Console.WriteLine("Середня оцiнка студента = " + sum / 5);
+    Console.WriteLine("Допуск до екзамену отримано");
+}
+else
+{
+    Console.WriteLine("Середня оцiнка студента = " + sum / 5);
+    Console.WriteLine("Допуску до екзамену немає");
+}
