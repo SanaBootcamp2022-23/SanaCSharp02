bd0a89c04fa400907f85030b8113617c17f4ce00
DmitryKalinovskyi <ipz223_kdm@student.ztu.edu.ua>
2022-12-26 16:51:22 +0200

    Initial commit

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PointsAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, x2, x3, x4, x5;
+
+            x1 = int.Parse(Console.ReadLine());
+            x2 = int.Parse(Console.ReadLine());
+            x3 = int.Parse(Console.ReadLine());
+            x4 = int.Parse(Console.ReadLine());
+            x5 = int.Parse(Console.ReadLine());
+
+            double average = ((float)x1 + x2 + x3 + x4 + x5) / 5;
+
+            if(average >= 4)
+            {
+                Console.WriteLine($"Student accepted! Average rating: {average}");
+            }
+            else
+                Console.WriteLine($"Student failed learning! Average rating: {average}");
+
+        }
+    }
+}
---
+++ SanaCSharp02/QuadraticEquation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            double D = b * b - 4 * a * c;
+
+            if (D > 0)
+            {
+                double x1 = (-b + Math.Sqrt(D)) / 2 / a;
+                double x2 = (-b - Math.Sqrt(D)) / 2 / a;
+
+                Console.WriteLine($"Solution x1 and x2: {x1} and {x2}");
+            }
+            else if (D == 0)
+            {
+                double x = -b / 2 / a; 
+
+                Console.WriteLine($"Solution x: {x}");
+            }
+            else
+                Console.WriteLine("Equation don't have real solution");
+
+        }
+    }
+}
