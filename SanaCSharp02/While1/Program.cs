b353eef29b1e69b72e7dcbce5dba6cdcc4085303
Andrii vovk <sana22_vam@student.ztu.edu.ua>
2022-12-27 00:25:39 +0200

    edited

--- SanaCSharp02/While1/Program.cs
+++ SanaCSharp02/While1/Program.cs
@@ -1,35 +1,14 @@
-﻿using System;
+﻿double n = 0, i = 1, a = 0, b = 0, c = 0, fact = 1;
 
-double a = 0, b = 0, c = 0, n, fact = 0;
-int i = 1;
+Console.Write("Input n:");
+n = double.Parse(Console.ReadLine());
 
-Console.Write("Enter n:");
-n = Convert.ToDouble(Console.ReadLine());
-
-
-while (i <= n)
+while (i <= n) 
 {
-    a = a + (Math.Pow(-1, n)) / (2 * n + 1);
+    a += Math.Pow(-1, i) / ((i * 2) + 1);
+    b += 1 + (1/Math.Pow(i,2));
+    fact = fact * i;
+    c += fact;
     i++;
 }
-Console.WriteLine($"a = {a}");
-
-i = 1;
-while (i <= n)
-{
-    b += 1 + 1 / Math.Pow(i, 2);
-    i++;
-}
-Console.WriteLine($"b = {b}");
-
-i = 1;
-while (i <= n)
-{
-    while (fact <= i)
-    {
-        c += fact;
-        fact++;
-    }
-    i++;
-}
-Console.WriteLine($"c = {c}");
+Console.WriteLine($" a = {a}\n b = {b}\n c = {c}\n");
