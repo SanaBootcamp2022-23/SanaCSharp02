b70c9a81b7250f8b10250c24d1f9265c6143e659
Elena Savchuk <sana22_soi@student.ztu.edu.ua>
2022-12-26 19:14:08 +0200

    task2

---
+++ PointsAverage/PointsAverage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace PointsAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            double summ = 0;
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+                summ += array[i];
+            }
+            double avarage = summ / 5;
+            Console.WriteLine(avarage);
+            if (avarage >= 4)
+            {
+                Console.WriteLine("Студент отримує допуск");
+            }
+            else
+            {
+                Console.WriteLine("Студент не отримує допуск");
+
+            }    
+        }
+    }
+}
