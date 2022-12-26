6ef96240ea1e9c95943dbe1bb80fccf9e93b7eb8
SashaZaitsev <ipz224_zoi@student.ztu.edu.ua>
2022-12-26 19:34:39 +0200

    SanaPrakt02

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,18 @@
+﻿double average = 0;
+for(int i = 0; i < 5; i++)
+{
+    Console.Write("Enter mark " + (i + 1) + ": ");
+    double degree = Convert.ToInt32(Console.ReadLine());
+    average += degree;
+}
+average /= 5;
+Console.WriteLine("Average is " + average);
+if (average >= 4)
+{
+    Console.WriteLine("Student passed");
+}
+else
+{
+    Console.WriteLine("Student didn`t pass");
+
+}
---
+++ SanaCSharp02/While1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double a = 0;
+double b = 0;
+double c = 0;
+int f = 1, fn = n;
+while (fn > 0)
+{
+    f *= fn;
+    fn--;
+}
+while (n >= 1)
+{
+    a += Math.Pow(-1, n) / (2 * n + 1);
+    b += 1 + 1 / (double)(n * n);
+    c += f;
+    f /= n;
+    n--;
+}
+Console.WriteLine("A = " + a);
+Console.WriteLine("B = " + b);
+Console.WriteLine("C = " + c);
