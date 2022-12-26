e35c2daf97603ecb4c13f3bc965f4f04e867fabc
Віктор Мінайленко <ipz224_mvyu@student.ztu.edu.ua>
2022-12-27 00:32:33 +0200

    Created project PointsAverage

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введіть 5 оцінок одного з студентів:");
+
+double[] array = new double[5];
+double avarage = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = double.Parse(Console.ReadLine());
+    avarage += array[i];
+}
+avarage /= array.Length;
+
+Console.WriteLine("Його середній бал = {0}", avarage);
+Console.WriteLine("{0}", avarage > 4 ? "Студент допущений до екзамену" : "Студент не допущений до екзамену");
