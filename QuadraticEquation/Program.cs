aeca4c823850507d1051603ea19783809e5ab1b3
Anatoliy Kolesnyk <uitpm221_kab@student.ztu.edu.ua>
2022-12-26 05:09:02 +0200

    Created QuadtraticEquation and complete task 1

---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,27 @@
+﻿double a, b, c;
+
+double D, x1, x2;
+
+Console.WriteLine("Please, inpute number a");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please, inpute number b");
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please, inpute number c");
+c = Convert.ToDouble(Console.ReadLine());
+
+D = Math.Pow(b, 2) - 4 * a * c;
+if(D==0)
+{
+    x1 = -b / (2.0 * a);
+    Console.WriteLine($"D={D} x1 = x2 = {x1}");
+}
+else if (D>0)
+{
+    x1 = (-b + Math.Sqrt(D)) / (2 * a);
+    x2 = (-b - Math.Sqrt(D)) / (2 * a);
+    Console.WriteLine($"D={D} x1 = {x1}\nx2 = {x2}");
+}
+else
+{
+    Console.WriteLine("There are no solutions");
+}
