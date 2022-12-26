805f44bd1acf8f7b4d379b5c6ccfa7d1f155108a
Viktoriia Haidamaka <sana22_gvo@student.ztu.edu.ua>
2022-12-26 18:25:50 +0200

    Fourth project done

---
+++ SanaCSharp02/While1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+int inputN;
+double resultA = 0, resultB = 0, resultC = 0;
+double factorial = 1;
+
+Console.Write("N = ");
+inputN = int.Parse(Console.ReadLine());
+
+int i = 1;
+
+while (i <= inputN)
+{
+    resultA += Math.Pow(-1.0, inputN) / (2.0 * inputN + 1);
+    resultB += (1 + 1.0 / Math.Pow(inputN, 2));
+    factorial *= i;
+    resultC += factorial;
+    i++;
+}
+
+Console.WriteLine($"a = {resultA}");
+Console.WriteLine($"b = {resultB}");
+Console.WriteLine($"c = {resultC}");
