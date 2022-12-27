b43e53ad445736f7023beaa8da67fe6b203a7445
Volodymyr Chystiakov <sana22_chvo@student.ztu.edu.ua>
2022-12-27 17:08:02 +0200

    Solution SanaCSharp02 created

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Math;
+
+double m1, m2, m3, m4, m5, av;
+
+Console.WriteLine("Enter 5 marks");
+m1 = Convert.ToDouble(Console.ReadLine());
+m2 = Convert.ToDouble(Console.ReadLine());
+m3 = Convert.ToDouble(Console.ReadLine());
+m4 = Convert.ToDouble(Console.ReadLine());
+m5 = Convert.ToDouble(Console.ReadLine());
+
+av = (m1 + m2 + m3 + m4 + m5) / 5;
+
+if (av >= 4)
+{
+    Console.WriteLine("Admitted to the exam.");
+}
+else
+{
+    Console.WriteLine("Not admitted to the exam.");
+}
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Math;
+
+double a, b, c, D, x1, x2;
+
+Console.WriteLine("Enter a = ");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter b = ");
+b = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter c = ");
+c = Convert.ToDouble(Console.ReadLine());
+
+D = b * b - 4 * a * c;
+
+if (D < 0)
+{
+    Console.WriteLine("The quadratic equation has no roots");
+}
+else if (D == 0)
+{
+    x1 = (-b + Sqrt(D)) / (2 * a);
+    Console.WriteLine("The quadratic equation has one root\nx={0}\n", x1);
+}
+else
+{
+    x1 = (-b + Sqrt(D)) / (2 * a);
+    x2 = (-b - Sqrt(D)) / (2 * a);
+    Console.WriteLine("The quadratic equation has two roots\nx1={0}\nx2={1}", x1, x2);
+}
