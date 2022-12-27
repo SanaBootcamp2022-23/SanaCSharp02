197d73a4426e9ef74f00385352c0e2910b5a518d
Anatolii Yarmolenko <ipz224_yaam@student.ztu.edu.ua>
2022-12-27 16:54:32 +0200

    Complete2

--- SanaCSharp02/While1/Program.cs
+++ SanaCSharp02/While1/Program.cs
@@ -3,32 +3,30 @@
 Console.OutputEncoding = Encoding.Unicode;
 Console.InputEncoding = Encoding.Unicode;
 
-int num, i, j, l;
-double a = 0, b = 0, c = 1;
+int i, j;
+double a = 0, b = 0, c = 0, dob = 1;
 
 Console.Write("Введіть кількість повторення циклу:");
-num = int.Parse(Console.ReadLine());
-
-i = num;
-j = num;
-l = num;
+i = int.Parse(Console.ReadLine());
 
 while (i > 0)
 {
     a += Math.Pow((-1), i) / (2 * i + 1);
+
+    b += 1 + (1 / Math.Pow(i, 2));
+
+    j = i;
+    dob = 1;
+
+    while (j > 1)
+    {
+        dob *= j;
+        j--;
+    }
+
+    c += dob;
     i--;
-}
 
-while (j > 0)
-{
-    b += 1 + (1 / Math.Pow(j, 2));
-    j--;
-}
-
-while (l > 1)
-{
-    c *= l;
-    l--;
 }
 
 Console.Write("\na = {0}", a);
