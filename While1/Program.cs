84a7be14428087e1f0d9622c7fbefdb8150d03f3
Yehor Panchenko <sana22_pyem@student.ztu.edu.ua>
2022-12-27 20:31:51 +0200

    Laba4C#

---
+++ While1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+double n=0;
+int step=1;
+double a = 0, b = 0, c = 0, fact = 1;
+
+
+
+Console.Write("Input number n:");
+n = double.Parse(Console.ReadLine());
+while (step<=n)
+{
+    a = a + Math.Pow(-1,n)/((2*n)+1);
+    b = b + ((1) + (1)/ Math.Pow(n, 2));
+
+    fact= fact*step;
+    c += fact; 
+
+    step++;
+    
+}
+Console.WriteLine($"a={a}");
+Console.WriteLine($"b={b}");
+Console.WriteLine($"c={c}");
