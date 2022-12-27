3c889d8e2408ea0af8bcf7ec9996af175806a9cc
Yurii Tsiurpita <mo65_tsyus@student.ztu.edu.ua>
2022-12-27 13:22:20 +0200

    Created project PointsAverage

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter 5 stedent grades:");
+double point = 0;
+point += double.Parse(Console.ReadLine());
+point += double.Parse(Console.ReadLine());
+point += double.Parse(Console.ReadLine());
+point += double.Parse(Console.ReadLine());
+point += double.Parse(Console.ReadLine());
+double avgPoint = point / 5;
+Console.WriteLine("Середній бал студента: " + avgPoint);
+if (avgPoint >= 4)
+{
+    Console.WriteLine("Студент допущений до екзамену");
+}
+if (avgPoint < 4)
+{
+    Console.WriteLine("Студент не допущений до екзамену");
+}
--- SanaCSharp02/QuadraticEquation/Program.cs
+++ SanaCSharp02/QuadraticEquation/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Math;
+int a, b, c;
+Console.Write("enter a:");
+a = int.Parse(Console.ReadLine());
+Console.Write("enter b:");
+b = int.Parse(Console.ReadLine());
+Console.Write("enter c:");
+c = int.Parse(Console.ReadLine());
+double d = b * b - 4 * a * c;
+Console.WriteLine($"D: {d}");
+if (d > 0)
+{
+    double x1 = (-b + Sqrt(d)) / (2 * a);
+    double x2 = (-b - Sqrt(d)) / (2 * a);
+    Console.WriteLine($"x1: {x1:F2}\nx2: {x2:F2}");
+}
+else if (d == 0)
+{
+    double x1 = (-b + Sqrt(d)) / (2 * a);
+    Console.WriteLine($"x1: {x1:F2}");
+}
+else
+{
+    Console.WriteLine("Рівняння розв'язків не має!");
+}
