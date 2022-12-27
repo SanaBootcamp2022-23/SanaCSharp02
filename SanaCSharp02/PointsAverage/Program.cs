4ea689ab8d85bb606c55a5ec3b9c7838612972f6
Dmytro Fedorovych <ipz213_fdm@student.ztu.edu.ua>
2022-12-28 00:01:43 +0200

    task done

---
+++ SanaCSharp02/PointsAverage/Program.cs
@@ -0,0 +1,13 @@
+﻿double average = 0, a;
+Console.WriteLine("Введіт 5 оцінок");
+for (int i = 1; i <= 5; i++)
+{
+    Console.Write(i + " Оцінка: ");
+    a = double.Parse(Console.ReadLine());
+    average = a + average;
+}
+average /= 5;
+if (average >= 4)
+    Console.WriteLine("Допуск");
+else
+    Console.WriteLine("Не допуск");
---
+++ SanaCSharp02/QuadraticEquation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("Введіть значення a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть значення b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть значення c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double D = Math.Pow(b, 2) - 4 * a * c;
+            if (D > 0)
+            {
+                double sD = Math.Sqrt(D);
+                Console.WriteLine($"Корень D: {sD}");
+                double x1 = (-b + sD) / (2 * a);
+                double x2 = (-b - sD) / (2 * a);
+                Console.WriteLine($"x1 = {x1}");
+                Console.WriteLine($"x2 = {x2}");
+            }
+            else if (D == 0)
+            {
+                double sD = Math.Sqrt(D);
+                Console.WriteLine($"Корень D: {sD}");
+                double x = -b / (2 * a);
+                Console.WriteLine($"x = {x}");
+            }
+            else if (D < 0)
+            {
+                Console.WriteLine("Коренів немає");
+            }
+        }
+    }
+}
