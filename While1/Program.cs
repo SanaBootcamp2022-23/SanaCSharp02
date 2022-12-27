9b23f94a7b1f1b767cff1ee14e3d2f9711981bf1
Vitalii Tushak <ipz224_tvs@student.ztu.edu.ua>
2022-12-27 17:55:03 +0200

    SanaCSharp02

---
+++ While1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace SanaCSharp02
+{
+    class Program
+    {
+        private static double а;
+
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            double a = 0, b = 0, c = 0, n;
+            Console.Write(" Введіть n:");
+            n = double.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                a += (Math.Pow(-1, i)) / (2 * i + 1);
+            }
+            Console.WriteLine($" а = {a}");
+            for (int i = 1; i < n; i++)
+            {
+                b += (1 + 1 / Math.Pow(i, 2));
+            }
+            Console.WriteLine($" b = {b}");
+            int fact = 1;
+            for(int i = 1; i <= n; i++, fact *= i)
+{
+                c += fact;
+            }
+            Console.WriteLine($" c = {c}");
+        }
+    }
+}
