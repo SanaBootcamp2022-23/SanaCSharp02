14dd1a5566b5a42c9505e23a5e5696770f04a23f
Artem Ivliev <sana22_iav@student.ztu.edu.ua>
2022-12-26 23:56:14 -0800

    all

---
+++ SanaCSharp02/QuadraticEquation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+double a;
+double b;
+double c;
+double x1;
+double x2;
+double discriminant;
+Console.Write("a=");
+a = double.Parse(Console.ReadLine());
+Console.Write("b=");
+b = double.Parse(Console.ReadLine());
+Console.Write("c=");
+c = double.Parse(Console.ReadLine());
+
+discriminant = Math.Pow(b, 2) - 4 * a * c;
+
+if (discriminant > 0)
+{
+    x1 = (-b + Math.Sqrt(discriminant)) / 2 * a;
+    x2 = (-b - Math.Sqrt(discriminant)) / 2 * a;
+    Console.WriteLine($"Дискримінант:{discriminant}");
+    Console.WriteLine($"Розв'язання:{x1},{x2}"); ;
+}
+else if (discriminant == 0)
+{
+    Console.WriteLine("Тільки 1 розв'язання");
+    x1 = (-b + Math.Sqrt(discriminant)) / 2 * a;
+    Console.WriteLine($"Дискримінант:{discriminant}");
+    Console.WriteLine($"Розв'язання:{x1}");
+
+}
+else if (discriminant < 0)
+{
+    Console.WriteLine("Немає розв'язання(");
+    Console.WriteLine($"Дискримінант:{discriminant}");
+
+}
+   
---
+++ SanaCSharp02/SwitchValues/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+int number;
+Console.WriteLine("Введіть число від 0 до 10");
+number = int.Parse(Console.ReadLine());
+
+switch (number)
+{
+    case 0:
+        Console.WriteLine("нуль");
+        break;
+    case 1:
+        Console.WriteLine("один");
+        break;
+    case 2:
+        Console.WriteLine("два");
+        break;
+    case 3:
+        Console.WriteLine("три");
+        break;
+    case 4:
+        Console.WriteLine("чотири");
+        break;
+    case 5:
+        Console.WriteLine("п'ять");
+        break;
+    case 6:
+        Console.WriteLine("шість");
+        break;
+    case 7:
+        Console.WriteLine("сім");
+        break;
+    case 8:
+        Console.WriteLine("вісім");
+        break;
+    case 9:
+        Console.WriteLine("дев'ять");
+        break;
+    case 10:
+        Console.WriteLine("десять");
+        break;
+}
