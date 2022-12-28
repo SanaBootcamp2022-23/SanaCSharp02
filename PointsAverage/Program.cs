9fb1644d046478860043825821c63844087bb41f
Elena Maksymchuk <sana22_mol@student.ztu.edu.ua>
2022-12-28 23:00:25 +0200

    All tasks completed

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+double sum = 0, averagePoint;
+
+for(int i = 0; i < 5; i++)
+{
+    Console.Write("Оцінка " + (i + 1) + ": ");
+    sum += double.Parse(Console.ReadLine());
+}
+averagePoint = sum / 5.0;
+Console.WriteLine("\nСередній балл: " + averagePoint);
+if(averagePoint >= 4)
+    Console.WriteLine("Студент допущений до екзамену.");
+else
+    Console.WriteLine("Студент не допущений до екзамену.");
---
+++ While1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+int n;
+double a = 0, b = 0, c = 0, factorial;
+
+Console.Write("Введіть число n: ");
+n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    a += Math.Pow(-1, i) / ((2 * i) + 1.0);
+    b += 1.0 + 1.0 / Math.Pow(i, 2);
+    factorial = 1;
+    for (int j = 1; j <= i; j++)
+    {
+        factorial *= j;
+    }
+    c += factorial;
+}
+
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+Console.WriteLine("c = " + c);
