fb1ce75cac00fb1f1489e3dd938aca275f76ba97
Valerii Piaskivskiy <sana22_pvo1@student.ztu.edu.ua>
2022-12-29 16:24:22 +0200

    Code optimized

--- While1/Program.cs
+++ While1/Program.cs
@@ -1,32 +1,25 @@
-﻿double n;
+﻿double n ;
 double resultA = 0;
 double resultB = 0;
 double resultC = 0;
-double factor = 1;
+
 
 Console.Write("Input number n = ");
 n = double.Parse(Console.ReadLine());
+
 for (double i = 1; i <= n; i++)
 {
     resultC += i;
-}
-Console.WriteLine($"c = {resultC}");
 
-
-for (double i = 1; i <= n; i++)
-{
     double calculateB = 1 + (1 / Math.Pow(i, 2));
     resultB += calculateB;
-}
-Console.WriteLine($"b = {resultB}");
-for (double i = 1; i <= n; i++)
-{
+
     double calculateA = Math.Pow(-1,i)/((2 * i) + 1);
     resultA += calculateA;
 }
 Console.WriteLine($"a = {resultA}");
+Console.WriteLine($"b = {resultB}");
+Console.WriteLine($"c = {resultC}");
 
 
 
-//(i*2)+1
-//factor *= i;//
