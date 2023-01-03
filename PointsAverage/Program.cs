4321bf5068e5f80cd5d128d941cf5fed5cbb6e8b
Oleksandr Hanchevskyi <ipz222_goo@student.ztu.edu.ua>
2023-01-03 17:27:05 +0300

    123

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,10 @@
+﻿double a , m = 0;
+for(int i = 0; i < 5; i++)
+{
+    a = double.Parse(Console.ReadLine());
+    m += a;
+}
+a = m / 5;
+if (a >= 4)
+    Console.WriteLine("Допуск");
+else Console.WriteLine("Недопуск");
---
+++ While1/Program.cs
@@ -0,0 +1,14 @@
+﻿double a, b, c, n;
+n = double.Parse(Console.ReadLine());
+a = 0;
+b = 0;
+c = 0;
+for (int i = 1; i <= n; i++)
+{
+    a += (Math.Pow(-1, n)) / (2 * n + 1);
+    b += (1 + 1 / Math.Pow(n, 2));
+    c += n;
+}
+Console.WriteLine("a = {0}", a);
+Console.WriteLine("b = {0}", b);
+Console.WriteLine("c = {0}", c);
