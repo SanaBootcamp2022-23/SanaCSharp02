3f11e88271c97bbf46df93b0b30f653e1c89464b
Ruslan Kuksa <ipz221_kro@student.ztu.edu.ua>
2023-01-04 04:44:21 +0200

    While

---
+++ SanaCSharp02/While1/Program.cs
@@ -0,0 +1,16 @@
+﻿int n;
+double a = 0, b = 0, c = 0;
+double fact = 1;
+Console.Write("Enter n: ");
+n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    a += Math.Pow(-1, i) / (2 * i + 1);
+    b += 1 + 1 / Math.Pow(i, 2);
+    fact *= i;
+    c += fact;
+}
+Console.WriteLine($"A: {a}");
+Console.WriteLine($"B: {b}");
+Console.WriteLine($"C: {c}");
