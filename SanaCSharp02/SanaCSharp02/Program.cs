25e9384c998c2ab72f2694bd4d563436972f8dfd
Koretskyi_Yura <ipz221_kyui@student.ztu.edu.ua>
2023-01-15 19:43:48 +0200

    Виконано практичну роботу

---
+++ SanaCSharp02/SanaCSharp02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            double D, x, x1, x2, a, b, c; ;
+            Console.Write("Введіть a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Введіть b: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Введіть c: ");
+            c = int.Parse(Console.ReadLine());
+            if (a == 0)
+            {
+                Console.WriteLine("Помилка!");
+                return;
+            }
+            else
+            {
+                Console.Write($"Квадратне рівняння: {a}x^2 + {b}x + {c} = 0.\n");
+                D =Math.Pow(b, 2) - 4 * a * c;
+                Console.Write($"Дискримінант = {D}\n");
+                if (D == 0)
+                {
+                    x = -b / (2 * a);
+                    Console.Write($"Рівняння має один корінь X = {x}");
+                }
+                else if (D > 0)
+                {
+                    x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                    Console.Write($"Рівняння має два корені: \nX1={x1}\nX2={x2}");
+                }
+                else
+                {
+                    Console.Write("Дискримінант менше 0. Рівняння не має жодного кореня!");
+                }
+            }
+        }
+    }
+}
---
+++ SanaCSharp02/While1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            int n, i;
+            double a = 0, b = 0, c = 0, factorial = 1;
+            Console.WriteLine("Введіть n: ");
+            n = int.Parse(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                a += Math.Pow(-1, i) / (2 * i + 1);
+                b += 1 + 1 / Math.Pow(i, 2);
+                factorial *= i;
+            }
+            Console.WriteLine($"Результат a: {a}");
+            Console.WriteLine($"Результат b: {b}");
+            Console.WriteLine($"Результат c: {factorial}");
+        }
+    }
+}
+
+
+
