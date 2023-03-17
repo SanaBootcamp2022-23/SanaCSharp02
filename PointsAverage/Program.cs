0dd6f7373d66d5bf89c6b2672268f8b3729d8819
Bordiuh Olha <kb211_boo@student.ztu.edu.ua>
2023-03-17 02:23:22 +0200

    points average

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] points = new int[5];
+int sum = 0, avg;
+Console.WriteLine("input");
+for (int i = 0; i < 5; i++)
+{
+    Console.Write(":");
+    points[i] = Convert.ToInt32(Console.ReadLine());
+    sum = sum + points[i];
+}
+avg = sum/5;
+if (avg>=4)
+    Console.WriteLine("pass");
+else 
+    Console.WriteLine("did not pass");
