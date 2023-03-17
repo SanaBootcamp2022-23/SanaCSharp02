cbaca3367c97e1ef2e024de085925dde99d1aef3
Bordiuh Olha <kb211_boo@student.ztu.edu.ua>
2023-03-17 04:45:28 +0200

    while 1

---
+++ While1/Program.cs
@@ -0,0 +1,19 @@
+﻿double a = 0, b = 0, c = 0;
+int i = 1, n, r = 1, j = 1;
+Console.Write("n:");
+n = Convert.ToInt32(Console.ReadLine());
+while (i <= n)
+{
+    a = a + Double.Pow(-1, n) / (2 * n + 1);
+    b = b + (1 + 1 / (n * n));
+    while (j <= i)
+    {
+        r = r * j;
+        j++;
+    }
+    c = c + r;
+    i++;
+    j = 1;
+    r = 1;
+}
+Console.WriteLine($"a = {a} \nb = {b} \nc = {c}");
